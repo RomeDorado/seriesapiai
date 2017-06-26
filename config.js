0f90486efd465b6fd42ccee11dde369069cc2423
RomeDorado <romedorado@gmail.com>
2017-06-26 13:48:54 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    FB_PAGE_TOKEN: 'EAAEoYbDg2dsBABqbHEsmdW3v1cfK2ZBGLoCZBwTsDwrUn0nDXk1Yo9oU5cymUDdPW7a1q9RQgENR02SMfbfDNt2pr0BurmoNfUT3wRiQmm9ObbhoR2QvrIyVZBxBNVmAMMOoja7jCzpecWUIe0RtowNvAiIy0ZBRZA5gyWMKzNAZDZD',
+    FB_VERIFY_TOKEN: 'seriestoken',
+    API_AI_CLIENT_ACCESS_TOKEN: '4bf6e1438dcb4a6880c5226db967d58a',
+    FB_APP_SECRET: 'de1d5a8b13df33b155f7966b66500284',
+    SERVER_URL: "https://floating-atoll-10304.herokuapp.com/",
+};
