482ea218e35e3ad7a390d2c264bf4b8a98ef7564
RomeDorado <romedorado@gmail.com>
2017-06-27 16:15:39 +0800

    agenda

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ module.exports = {
     API_AI_CLIENT_ACCESS_TOKEN: '4bf6e1438dcb4a6880c5226db967d58a',
     FB_APP_SECRET: 'de1d5a8b13df33b155f7966b66500284',
     SERVER_URL: "https://floating-atoll-10304.herokuapp.com/",
-    MONGO_URI: "mongodb://127.0.0.1/seriesbot"
+    MONGO_URI: "mongodb://heroku_fccgb73z:sqhl8pplvoavm9s9t6nk7sd20b@ds139322.mlab.com:39322/heroku_fccgb73z"
 };
