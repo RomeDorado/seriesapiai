430fede614688b24c0db36743727d91ec83231da
RomeDorado <romedorado@gmail.com>
2017-06-27 15:45:12 +0800

    agenda

--- app.js
+++ app.js
@@ -242,12 +242,12 @@ function handleApiAiAction(sender, action, responseText, contexts, parameters) {
 				}
 				
 				agenda.now('createReminder', {
-				sender,
+				fbid,
 				datetime: datetime,
-				task: context.task
+				task: "watch movie"
 				});
 				
-				createReminderAgenda();
+				createReminderAgenda(sender);
 					console.log(tvshow + " this is the tv show");
 				}
 			return contextObj;
@@ -261,6 +261,53 @@ function handleApiAiAction(sender, action, responseText, contexts, parameters) {
 			sendTextMessage(sender, responseText);
 	}
 }
+
+function createReminderAgenda(sender){
+	return agenda.define('createReminder', job => {
+    // Extract fbid, datetime and task from job
+    const {fbid, datetime, task} = job.attrs.data;
+
+    // Get the FB User's timezone
+    getProfile(fbid)
+      .then(profile => {
+        const {first_name, timezone} = profile;
+        // Calculating the timezone offset datetime
+        const UTC_Offset = moment.utc(datetime).subtract(timezone, 'hours');
+        // Calculating the difference between now and the UTC_Offset datetime. If this is
+        // 0 or below then we can use the UTC_datetime directly OR else use UTC_Offset
+        const timeDiff = UTC_Offset.diff(moment.utc());
+        // If timeDiff is 0 or below, then use UTC_datetime or else use UTC_Offset. also convert to date.
+        const scheduleTime = (timeDiff <= 0 ? moment.utc(datetime) : UTC_Offset).toDate();
+        // Setup the job
+        agenda.schedule(scheduleTime, 'reminder', {
+          fbid,
+          first_name,
+          task
+        });
+      })
+      .catch(error => console.log(error));
+    // Compute an offset from UTC before scheduling the task
+  });
+
+}
+
+function getProfile(id) {
+		return new Promise((resolve, reject) => {
+			request({
+				uri: `https://graph.facebook.com/v2.7/${id}`,
+				qs: {
+					access_token: this.PAGE_ACCESS_TOKEN
+				},
+				method: 'GET'
+			}, (error, response, body) => {
+				if(!error & response.statusCode === 200) {
+					resolve(JSON.parse(body));
+				} else {
+					reject(error);
+				}
+			});
+		});
+	}
 
 var check = false;
 
