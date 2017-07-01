a42e89ace45648e1dcc1551a1abf8f880367314a
RomeDorado <romedorado@gmail.com>
2017-07-01 23:02:20 +0800

    asd

--- app.js
+++ app.js
@@ -173,13 +173,43 @@ function receivedMessage(event) {
 		//send message to api.ai
 		sendToApiAi(senderID, messageText);
 	} else if (messageAttachments) {
+	
 		handleMessageAttachments(messageAttachments, senderID);
 	}
 }
 
+const Speech = require('@google-cloud/speech');
+const speech = Speech();
+
 
 function handleMessageAttachments(messageAttachments, senderID){
+		let {
+			type,
+			payload :{
+				url
+			}
+		} = messageAttachments;
 	//for now just reply
+	const gcsUri = url;
+	const encoding = 'LINEAR16'
+	const sampleRateHertz = 16000;
+	const languageCode = 'en-US';
+	const request = {
+  encoding: encoding,
+  sampleRateHertz: sampleRateHertz,
+  languageCode: languageCode
+};
+
+// Detects speech in the audio file
+speech.recognize(gcsUri, request)
+  .then((results) => {
+    const transcription = results[0];
+
+    console.log(`Transcription: ${transcription}`);
+  })
+  .catch((err) => {
+    console.error('ERROR:', err);
+  });
 	sendTextMessage(senderID, "Attachment received. Thank you.");
 	console.log(JSON.stringify(messageAttachments) + "This is the attachment");
 }
