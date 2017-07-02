7dec90ad43ebac2a17c88949004c5cdb58e167f4
RomeDorado <romedorado@gmail.com>
2017-07-02 20:07:44 +0800

    cards

--- app.js
+++ app.js
@@ -2201,7 +2201,7 @@ function receivedPostback(event) {
 
   if(payload.includes("card")){
     let recTitle = payload.split("/")[1];
-		let category = payload.split("/")[2];
+		category = payload.split("/")[2];
     tvshow = recTitle;
     let intents = "posters";
     payload = "card";
