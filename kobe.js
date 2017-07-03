bed48104f3f60e02b6bf0be37a167895e33b7d10
RomeDorado <romedorado@gmail.com>
2017-07-03 16:53:13 +0800

    'asd

--- kobe.js
+++ kobe.js
@@ -14,3 +14,20 @@ var title = "kobe"
  let secondname = text.split("/")[2];
 
  console.log("firstname: "+ firstname + " second name: " +secondname);
+
+ var trim = require('trim')
+var wtj = require('website-to-json')
+ 
+wtj.extractUrl('http://www.imdb.com/news/tv', {
+  fields: ['data'],
+  parse: function($) {
+    return {
+      title: trim($(".title_wrapper h1").text()),
+      image: $(".poster img").attr('src'),
+      summary: trim($(".plot_summary .summary_text").text())
+    }
+  }
+})
+.then(function(res) {
+  console.log(JSON.stringify(res, null, 2));
+});
